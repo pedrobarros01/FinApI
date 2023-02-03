0af17031d526aab378b91cf11666ee6593bd001a
Pedro Barros <pedrobarros0101@outlook.com>
2023-02-03 09:53:15 -0300

    [FEAT][FINAPI] - Realizando o primeiro requisito e regra de negocio

--- src/index.js
+++ src/index.js
@@ -1,4 +1,33 @@
 const express = require('express');
+const {v4: uuidv4} = require("uuid");
 const app = express();
+
+app.use(express.json());
+
+const customers = [];
+/* 
+    cpf -> string
+    name -> string
+    id -> uuid
+    statement -> [] 
+*/
+
+app.post("/account", (request, response) => {
+    const {cpf, name} = request.body;
+    const customersAlreadyExists = customers.some((customer) => customer.cpf === cpf);
+
+    if(customersAlreadyExists){
+        return response.status(400).json({error: "Customer Alredy Exists!"});
+    }
+    customers.push({
+        cpf,
+        name,
+        id: uuidv4(),
+        statement: []
+    });
+
+    return response.status(201).send();
+    
+});
 
 app.listen(1313)
