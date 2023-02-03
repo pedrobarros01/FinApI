10d07af4261c5cae5130c0b89144fa487c3eb48f
Pedro Barros <pedrobarros0101@outlook.com>
2023-02-03 10:31:49 -0300

    [FEAT][FINAPI] - Realizando requisito e regra de deposito

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+/* 
+    cpf -> string
+    name -> string
+    id -> uuid
+    statement -> [] 
+*/
 const express = require('express');
 const {v4: uuidv4} = require("uuid");
 const app = express();
@@ -5,12 +11,18 @@ const app = express();
 app.use(express.json());
 
 const customers = [];
-/* 
-    cpf -> string
-    name -> string
-    id -> uuid
-    statement -> [] 
-*/
+
+//MiddleWare
+function verifyIfExistsAccountCpf(request, response, next){
+    const {cpf} = request.headers;
+    const customer = customers.find(customer => customer.cpf === cpf);
+    if(!customer){
+        return response.status(400).json({error: "Customer Not Found!"})
+    }
+    request.customer = customer;
+    return next();
+}
+
 
 app.post("/account", (request, response) => {
     const {cpf, name} = request.body;
@@ -29,5 +41,26 @@ app.post("/account", (request, response) => {
     return response.status(201).send();
     
 });
+//app.use(middleWare) - >todas as rotas utilizam
+//app.get("/recurso", middleware1, (req, res) => {}) -> so essa rota utiliza
+
+app.get("/statement", verifyIfExistsAccountCpf,  (request, response) => {
+    const { customer } = request;
+    
+    return response.json(customer.statement);
+});
+
+app.post("/deposit", verifyIfExistsAccountCpf, (request, response) => {
+    const {description, ammount} = request.body;
+    const {customer} = request;
+    const statementOperation = {
+        description,
+        ammount,
+        created_at: new Date(),
+        type: "credit"
+    }
+    customer.statement.push(statementOperation);
+    return response.status(201).send();
+})
 
 app.listen(1313)
